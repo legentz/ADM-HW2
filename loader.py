d8601c09415d520e34b311bce8649b6e8d8a09b7
legent <leandrogentili@gmail.com>
2018-11-03 17:15:22 +0100

    Loader is now working with the generator and the merged csv (lookup)

--- loader.py
+++ loader.py
@@ -1,16 +1,23 @@
 import pandas as pd
 import os
 
+COMMA = ','
+TAB = '\t'
+SPACE = ' '
+
 class Loader:
-	def __init__(self, csv=False, nrows=None, chunksize=10000, separator=','):
+	def __init__(self, csv=None, nrows=None, chunksize=10000, separator=COMMA):
 
 		# check csv path
-		assert csv != False and os.path.isfile(csv)
+		assert os.path.isfile(csv)
 
 		self.csv = csv
 		self.chunksize = chunksize
 		self.separator = separator
 		self.nrows = nrows
+		self.to_merge = None
+		self.to_merge_direction = None
+		self.to_merge_on = None
 
 	# get line with columns
 	def get_columns(self, nline=0):
@@ -18,6 +25,22 @@ class Loader:
 			for i, line in enumerate(f):
 				if i == nline:
 					return line.rstrip('\n').split(self.separator)
+
+	# this could not be iterated
+	def merge(self, csv=None, on=None, direction='left', usecols=[], separator=COMMA, drop_on_columns=False):
+
+		# check csv path
+		assert os.path.isfile(csv)
+		assert type(on) == tuple and len(on) > 0
+
+		if len(usecols) > 0:
+			self.to_merge = pd.read_csv(csv, usecols=usecols, sep=separator)
+		else:
+			self.to_merge = pd.read_csv(csv)
+
+		self.to_merge_direction = direction
+		self.to_merge_on = on
+		self.drop_on_columns = drop_on_columns
 
 	# get all parsed content
 	def iterate(self, usecols=[]):
@@ -30,4 +53,15 @@ class Loader:
 		else:
 			iterator = pd.read_csv(self.csv, nrows=self.nrows, chunksize=self.chunksize, sep=self.separator, iterator=True, usecols=usecols)
 
-		return iterator
+		# create a generator
+		for df in iterator:
+
+			# if something has to be merged
+			if self.to_merge is not None:
+				df = pd.merge(df, self.to_merge, how=self.to_merge_direction, left_on=self.to_merge_on[0], right_on=self.to_merge_on[1])
+
+				# remove columns that we're used to join
+				if self.drop_on_columns:
+					df = df.drop(columns=list(self.to_merge_on))
+
+			yield df
